d9b7f2e6a15dece68a8f463fbdc587c8818ec39f
codieorberson <codieorberson@gmail.com>
2023-09-11 15:56:53 -0500

    Add years of experience

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -44,56 +44,56 @@ export class SkillsComponent {
   }
 
   languages = [
-    { name: '.NET', progress: 95, details: '.NET Standard 2.0-2.1, .NET Framework 3.5-4.8, .NETCore 2.2 - 3.1, .NET 5 - 7' },
-    { name: 'Angular', progress: 90, details: 'AngularJS (1.x) and Angular 7-15, including reactive programming and RxJS.' },
-    { name: 'Python', progress: 85, details: 'Experience with Python 2.x and 3.x. Familiar with frameworks like Django and Flask.' },
-    { name: 'Pulumi', progress: 90, details: 'Infrastructure as Code tool for creating, deploying, and managing cloud infrastructure across Azure Resources.' },
-    { name: 'Java', progress: 80, details: 'Experience with Java SE and EE, along with frameworks like Spring Boot and Maven.' },
-    { name: 'JavaScript', progress: 90, details: 'ES5, ES6+ features, asynchronous operations with Promises, async/await, and AJAX requests.' },
-    { name: 'Apex', progress: 75, details: 'Salesforce Apex programming for triggers, batch jobs, and integration with Salesforce Lightning.' },
+    { name: '.NET', yearsExperience: 4, progress: 95, details: '.NET Standard 2.0-2.1, .NET Framework 3.5-4.8, .NETCore 2.2 - 3.1, .NET 5 - 7' },
+    { name: 'Angular', yearsExperience: 4, progress: 90, details: 'AngularJS (1.x) and Angular 7-15, including reactive programming and RxJS.' },
+    { name: 'Python', yearsExperience: 4, progress: 85, details: 'Experience with Python 2.x and 3.x. Familiar with frameworks like Django and Flask.' },
+    { name: 'Pulumi', yearsExperience: 3, progress: 90, details: 'Infrastructure as Code tool for creating, deploying, and managing cloud infrastructure across Azure Resources.' },
+    { name: 'Java', yearsExperience: 2, progress: 80, details: 'Experience with Java SE and EE, along with frameworks like Spring Boot and Maven.' },
+    { name: 'JavaScript', yearsExperience: 4, progress: 90, details: 'ES5, ES6+ features, asynchronous operations with Promises, async/await, and AJAX requests.' },
+    { name: 'Apex', yearsExperience: 2, progress: 75, details: 'Salesforce Apex programming for triggers, batch jobs, and integration with Salesforce Lightning.' },
 ];
 
   servers = [
-    { name: 'Azure', progress: 95, details: 'Microsofts cloud computing platform providing a variety of cloud services, including compute, analytics, storage, and networking.' },
-    { name: 'Windows', progress: 90, details: 'Microsoft Windows server operating systems used for enterprise environments, familiar with Active Directory, IIS, and other server roles.' },
-    { name: 'PCF', progress: 85, details: 'Pivotal Cloud Foundry: A cloud-native platform for deploying and scaling applications, with support for containers and microservices architectures.' },
-    { name: 'Linux', progress: 80, details: 'Open-source Unix-like operating system. Experience in configuring, managing, and maintaining Linux servers, familiar with distributions like Ubuntu, and Raspbian.' },
-    { name: 'Salesforce', progress: 95, details: 'Managed Salesforce integrations, specializing in REST/SOAP services and utilizing ADF to synchronize on-prem data, while ensuring the security of databases that depend on Salesforce data.' },
-    { name: 'Okta', progress: 95, details: 'Team Manages SSO using Okta, with proficiency in Okta Workflows, branding, and the integration of Event Hooks.' },
-    { name: 'Azure Data Factory', progress: 95, details: 'Data integration service in Azure that enables hybrid ETL, ELT, and data integration scenarios.' },
+    { name: 'Azure', yearsExperience: 4, progress: 95, details: 'Microsofts cloud computing platform providing a variety of cloud services, including compute, analytics, storage, and networking.' },
+    { name: 'Windows', yearsExperience: 4, progress: 90, details: 'Microsoft Windows server operating systems used for enterprise environments, familiar with Active Directory, IIS, and other server roles.' },
+    { name: 'PCF', yearsExperience: 3, progress: 85, details: 'Pivotal Cloud Foundry: A cloud-native platform for deploying and scaling applications, with support for containers and microservices architectures.' },
+    { name: 'Linux', yearsExperience: 4, progress: 80, details: 'Open-source Unix-like operating system. Experience in configuring, managing, and maintaining Linux servers, familiar with distributions like Ubuntu, and Raspbian.' },
+    { name: 'Salesforce', yearsExperience: 3, progress: 95, details: 'Managed Salesforce integrations, specializing in REST/SOAP services and utilizing ADF to synchronize on-prem data, while ensuring the security of databases that depend on Salesforce data.' },
+    { name: 'Okta', yearsExperience: 3, progress: 95, details: 'Team Manages SSO using Okta, with proficiency in Okta Workflows, branding, and the integration of Event Hooks.' },
+    { name: 'Azure Data Factory', yearsExperience: 3, progress: 95, details: 'Data integration service in Azure that enables hybrid ETL, ELT, and data integration scenarios.' },
 
   ];
 
   databases = [
-    { name: 'SQL Server', progress: 95, details: 'Experienced in designing, querying, and optimizing databases, working with stored procedures, triggers, and complex objects.' },
-    { name: 'Azure SQL', progress: 95, details: 'Skilled in deploying, scaling, and monitoring Azure SQL databases.' },
-    { name: 'SQL Server Profiler', progress: 90, details: 'A tool for monitoring and diagnosing SQL Server performance issues. Familiar with capturing and analyzing SQL Server events.' },
-    { name: 'Change Data Capture (CDC)', progress: 95, details: 'A feature in SQL Server for capturing insertions, updates, and deletions applied to SQL Server tables, and making this change data available within an Azure Service Bus' },
+    { name: 'SQL Server', yearsExperience: 4, progress: 95, details: 'Experienced in designing, querying, and optimizing databases, working with stored procedures, triggers, and complex objects.' },
+    { name: 'Azure SQL', yearsExperience: 3, progress: 95, details: 'Skilled in deploying, scaling, and monitoring Azure SQL databases.' },
+    { name: 'SQL Server Profiler', yearsExperience: 3, progress: 90, details: 'A tool for monitoring and diagnosing SQL Server performance issues. Familiar with capturing and analyzing SQL Server events.' },
+    { name: 'Change Data Capture (CDC)', yearsExperience: 3, progress: 95, details: 'A feature in SQL Server for capturing insertions, updates, and deletions applied to SQL Server tables, and making this change data available within an Azure Service Bus' },
   ];
 
   cicd = [
-    { name: 'Docker', progress: 95, details:  "Proficient in containerization using Docker, optimizing application deployment, and managing containerized services." },
-    { name: 'ADO (Azure DevOps)', progress: 95, details: 'Experienced with Azure DevOps (ADO) for continuous integration and delivery, source code management, and agile project tracking.' },
-    { name: 'Jenkins', progress: 90, details: 'Well-acquainted with Jenkins for building, testing, and deploying code automatically, ensuring streamlined CI/CD pipelines.' },
-    { name: 'Octopus', progress: 95, details: 'Skilled in using Octopus Deploy for automated deployment and release management, facilitating smooth software delivery processes.' },
+    { name: 'Docker', yearsExperience: 3, progress: 95, details:  "Proficient in containerization using Docker, optimizing application deployment, and managing containerized services." },
+    { name: 'ADO (Azure DevOps)', yearsExperience: 4, progress: 95, details: 'Experienced with Azure DevOps (ADO) for continuous integration and delivery, source code management, and agile project tracking.' },
+    { name: 'Jenkins', yearsExperience: 3, progress: 90, details: 'Well-acquainted with Jenkins for building, testing, and deploying code automatically, ensuring streamlined CI/CD pipelines.' },
+    { name: 'Octopus', yearsExperience: 3, progress: 95, details: 'Skilled in using Octopus Deploy for automated deployment and release management, facilitating smooth software delivery processes.' },
   ];
 
   apiTesting = [
-    { name: 'Postman', progress: 90, details: 'Proficient in using Postman for API testing, database design, and optimizing complex database operations.' },
-    { name: 'Insomnia', progress: 95, details: 'Expertise in leveraging Insomnia for API testing and monitoring Azure SQL databases.' },
-    { name: 'Soap UI', progress: 90, details: 'Skilled in utilizing Soap UI for SQL Server performance diagnosis and event analysis.' },
-    { name: 'REST / SOAP', progress: 90, details: 'Developed both RESTful and SOAP APIs with tools including Postman and Insomnia.' },
-    { name: 'Swagger', progress: 90, details: 'Experienced in using Swagger for API specification, documentation generation, and SDK development.' },
-    { name: 'Blazor', progress: 80, details: 'Adept at using Blazor for CI/CD, build pipeline management, and automation tasks.' },
+    { name: 'Postman', yearsExperience: 2, progress: 90, details: 'Proficient in using Postman for API testing, database design, and optimizing complex database operations.' },
+    { name: 'Insomnia', yearsExperience: 3, progress: 95, details: 'Expertise in leveraging Insomnia for API testing and monitoring Azure SQL databases.' },
+    { name: 'Soap UI', yearsExperience: 3, progress: 90, details: 'Skilled in utilizing Soap UI for SQL Server performance diagnosis and event analysis.' },
+    { name: 'REST / SOAP', yearsExperience: 5, progress: 90, details: 'Developed both RESTful and SOAP APIs with tools including Postman and Insomnia.' },
+    { name: 'Swagger', yearsExperience: 3, progress: 90, details: 'Experienced in using Swagger for API specification, documentation generation, and SDK development.' },
+    { name: 'Blazor', yearsExperience: 3,  progress: 80, details: 'Adept at using Blazor for CI/CD, build pipeline management, and automation tasks.' },
   ];
 
   unitTesting = [
-    { name: 'NUnit', progress: 95, details: 'Proficient in utilizing NUnit for robust backend .NET testing, ensuring system reliability and optimizing code performance' },
-    { name: 'LINQPad', progress: 95, details: 'Proficient in leveraging LINQPad for rapid testing and optimization of LINQ queries and quick .NET coding tasks, bypassing the need for extensive IDE setup.' },
-    { name: 'Moq', progress: 95, details: 'Experienced in leveraging Moq for effective mocking during unit tests, simulating external dependencies and ensuring isolated testing scenarios in .NET applications.' },
-    { name: 'MSTest', progress: 90, details: 'Proficient with MSTest for creating, executing, and analyzing unit tests within .NET applications, ensuring code reliability and quality through systematic testing practices.' },
-    { name: 'Cypress', progress: 90, details: 'Proficient in using Cypress for end-to-end testing of Angular applications, ensuring robust and responsive frontend performance.' },
-    { name: 'Karma / Jasmine', progress: 90, details: 'Experienced in leveraging Karma and Jasmine for unit testing Angular components, ensuring code quality and functionality consistency.' },
+    { name: 'NUnit', yearsExperience: 4, progress: 95, details: 'Proficient in utilizing NUnit for robust backend .NET testing, ensuring system reliability and optimizing code performance' },
+    { name: 'LINQPad', yearsExperience: 4, progress: 95, details: 'Proficient in leveraging LINQPad for rapid testing and optimization of LINQ queries and quick .NET coding tasks, bypassing the need for extensive IDE setup.' },
+    { name: 'Moq', yearsExperience: 3, progress: 95, details: 'Experienced in leveraging Moq for effective mocking during unit tests, simulating external dependencies and ensuring isolated testing scenarios in .NET applications.' },
+    { name: 'MSTest', yearsExperience: 3, progress: 90, details: 'Proficient with MSTest for creating, executing, and analyzing unit tests within .NET applications, ensuring code reliability and quality through systematic testing practices.' },
+    { name: 'Cypress', yearsExperience: 3, progress: 90, details: 'Proficient in using Cypress for end-to-end testing of Angular applications, ensuring robust and responsive frontend performance.' },
+    { name: 'Karma / Jasmine', yearsExperience: 3, progress: 90, details: 'Experienced in leveraging Karma and Jasmine for unit testing Angular components, ensuring code quality and functionality consistency.' },
   ];
 
   allSkills = [
