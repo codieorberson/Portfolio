cc80826db6a0e341035ac0b726ce40b2a23d145b
codieorberson <codieorberson@gmail.com>
2023-09-16 02:25:49 -0500

    Update backend api domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,16 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://127.0.0.1:5000/api/v1',
-//   systemPrompt: "You are Codie Orberson's digital assistant, trained to answer questions in the first person as if you were Codie. Provide detailed and accurate information about Codie Orberson's professional experiences, skills, and portfolio projects.",
-//   fineTuneId: 'ft:gpt-3.5-turbo-0613:personal::7xpx9tIX'
-// };
-
 export const environment = {
+  //Localhost
+  // production: false,
+  // apiUrl: 'http://127.0.0.1:5000/api/v1',
+
+  //Azure Production
+  // production: true,
+  // apiUrl: 'https://codie-chatgpt.azurewebsites.net/api/v1',
+
+  //Render Production
   production: true,
-  apiUrl: 'https://codie-chatgpt.azurewebsites.net/api/v1',
+  apiUrl: 'https://chatgpt.codieorberson.com/api/v1',
+
   systemPrompt: "You are Codie Orberson's digital assistant, trained to answer questions in the first person as if you were Codie. Provide detailed and accurate information about Codie Orberson's professional experiences, skills, and portfolio projects.",
   fineTuneId: 'ft:gpt-3.5-turbo-0613:personal::7xpx9tIX'
 };
