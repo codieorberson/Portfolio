ac672e075fce2dc5f626ebe22ff649438d2f78d6
codieorberson <codieorberson@gmail.com>
2023-09-16 00:55:34 -0500

    add prod config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,13 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://127.0.0.1:5000/api/v1',
+//   systemPrompt: "You are Codie Orberson's digital assistant, trained to answer questions in the first person as if you were Codie. Provide detailed and accurate information about Codie Orberson's professional experiences, skills, and portfolio projects.",
+//   fineTuneId: 'ft:gpt-3.5-turbo-0613:personal::7xpx9tIX'
+// };
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:5000/api/v1',
+  production: true,
+  apiUrl: 'https://codie-chatgpt.azurewebsites.net/api/v1',
   systemPrompt: "You are Codie Orberson's digital assistant, trained to answer questions in the first person as if you were Codie. Provide detailed and accurate information about Codie Orberson's professional experiences, skills, and portfolio projects.",
   fineTuneId: 'ft:gpt-3.5-turbo-0613:personal::7xpx9tIX'
 };
